5e73de24015b748799efe2b9c046dab22dcb855a
snutakki14 <snutakki14@evoketechnologies.com>
2023-09-29 12:00:39 +0530

    implemented pull to refresh in Feeds Screen

--- lib/app/services/authentication_service.dart
+++ lib/app/services/authentication_service.dart
@@ -1,63 +1,78 @@
-import 'dart:io' show Platform;
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:io';
 
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class AuthenticationService {
-  Future<void> login( Function(bool isSucess) onloginComplition) async {
-  
-      // Sign in with SAML identity provider
-      safePrint('Before Login');
-      if (Platform.isIOS) {
-        final result = await Amplify.Auth.signInWithWebUI(
-          provider: const AuthProvider.saml("EvokeAzureAD"),
-          options: const SignInWithWebUIOptions(
-            pluginOptions: CognitoSignInWithWebUIPluginOptions(
-              isPreferPrivateSession: true,
-            ),
+  FlutterWebviewPlugin _webviewPlugin = FlutterWebviewPlugin();
+
+  Future<void> login(
+      Function(bool isSucess) onloginComplition, BuildContext context) async {
+    safePrint('Before Login');
+
+    if (Platform.isIOS) {
+      final result = await Amplify.Auth.signInWithWebUI(
+        provider: const AuthProvider.saml("EvokeAzureAD"),
+        options: const SignInWithWebUIOptions(
+          pluginOptions: CognitoSignInWithWebUIPluginOptions(
+            isPreferPrivateSession: true,
           ),
-        );
-        safePrint('Result: $result');
-        onloginComplition(true);
-      } else {
-        // final result = await Amplify.Auth.signInWithWebUI(
-        //   provider: const AuthProvider.saml("EvokeAzureAD"),
-        // );
+        ),
+      );
+      safePrint('Result: $result');
+      onloginComplition(true);
+    } else {
+      final result = await Amplify.Auth.signInWithWebUI(
+        provider: const AuthProvider.saml("EvokeAzureAD"),
+      );
+      safePrint('Result: $result');
+      onloginComplition(true);
 
-        try {
-  final result = await Amplify.Auth.signInWithWebUI(
-    provider: const AuthProvider.saml("EvokeAzureAD"),
-  );
-  safePrint('Result: $result');
-} catch (e) {
-  safePrint('Error: $e');
-}
-        
-        // safePrint('Result: $result');
-      }
-    } 
-  }
 
-  // Launch a URL
-_launchURL() async {
-  const url = 'nexusapp://example.com'; // Replace with your URL
-  if (await canLaunchUrl(url as Uri)) {
-    await launchUrl(url as Uri);
-  } else {
-    throw 'Could not launch $url';
-  }
-}
+      
+      // Navigator.pop(context);
 
-  Future<void> signOutCurrentUser() async {
-    final result = await Amplify.Auth.signOut();
-    if (result is CognitoCompleteSignOut) {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.remove('token');
-      safePrint('Sign out completed successfully');
-    } else if (result is CognitoFailedSignOut) {
-      safePrint('Error signing user out: ${result.exception.message}');
+      // final result = await Amplify.Auth.signInWithWebUI(
+      //   provider: const AuthProvider.saml("EvokeAzureAD"),
+      // );
+
+      // safePrint('Result: $result');
+      // onloginComplition(true);
+      // if (result.isSignedIn) {
+      //   _webviewPlugin.close();
+      // } else {
+      //   print("User is not signed in.");
+      // }
+
+      // Close the WebView when the login is successful
+      // if (result.isSignedIn) {
+      //   _webviewPlugin.clearCache();
+
+      //   _webviewPlugin.close();
+      // }
+
+//       if (result.isSignedIn) {
+//         // Introduce a delay before closing the WebView
+//         await Future.delayed(
+//             Duration(seconds: 1)); // Adjust the duration as needed
+//         _webviewPlugin.close();
+// }
     }
   }
+}
 
+Future<void> signOutCurrentUser() async {
+  final result = await Amplify.Auth.signOut();
+  if (result is CognitoCompleteSignOut) {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    safePrint('Sign out completed successfully');
+  } else if (result is CognitoFailedSignOut) {
+    safePrint('Error signing user out: ${result.exception.message}');
+  }
+}
